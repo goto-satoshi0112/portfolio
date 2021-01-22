edf1a9dc3b7ae79df890b8d3bc575f7646c677d3
goto-satoshi0112 <73925600+goto-satoshi0112@users.noreply.github.com>
2021-01-23 06:30:59 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(function() {
+  var topHeight = $('.top').outerHeight(); //fvの高さを取得。
+  var headerHeight = $('.header').height(); //ヘッダーの高さを取得。
+  var spHeight = 30; //スマホサイズの余白の高さを取得。
+  var pcHeight = 80; //pcサイズの余白の高さを取得。
+  var headerNavMenu = $('.middle__nav-menu'); //ヘッダーコンテンツ
+
+  if ($(window).width() > 767) {
+    $(window).on('load scroll', function() {
+      if ($(window).scrollTop() < (topHeight - pcHeight)) {
+      //メインビジュアル内にいるので、フェードイン。
+      $('.middle').fadeOut();
+    } else {
+      //メインビジュアルより下までスクロールしたので、フェードアウト
+      $('.middle').fadeIn();
+    } 
+  });
+} else {
+  $(window).on('load scroll', function() {
+    if ($(window).scrollTop() < (headerHeight + spHeight)) {
+      //メインビジュアル内にいるので、フェードイン
+      $('.middle').fadeOut();
+    } else {
+      //メインビジュアルより下までスクロールしたので、フェードアウト
+      $('.middle').fadeIn();
+      }
+    });
+  }
+});
